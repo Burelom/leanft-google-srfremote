17f5386d0b92d6568ce24d7944487827f7fd9452
panama69 <github@flynnshome.com>
2018-04-04 00:45:33 -0500

    added verification

--- src/main/java/net/mf/LeanFtTest.java
+++ src/main/java/net/mf/LeanFtTest.java
@@ -2,6 +2,9 @@ package net.mf;
 
 import static org.junit.Assert.*;
 
+import com.hp.lft.report.ReportException;
+import com.hp.lft.report.Reporter;
+import com.hp.lft.report.Status;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,6 +19,8 @@ import unittesting.*;
 public class LeanFtTest extends UnitTestClassBase {
 
     private static Browser browser;
+    private static String myString = "StormRunner Functional";
+
     public LeanFtTest() {
         //Change this constructor to private if you supply your own public constructor
     }
@@ -43,21 +48,27 @@ public class LeanFtTest extends UnitTestClassBase {
     }
 
     @Test
-    public void test() throws GeneralLeanFtException {
+    public void test() throws GeneralLeanFtException, ReportException {
         browser.navigate("www.google.com");
         EditField field = browser.describe(EditField.class, new EditFieldDescription.Builder()
                 .name("q")
                 .tagName("INPUT")
                 .type("text").build());
 
-        field.setValue ("StormRunner Functional");
+        field.setValue (myString);
 
         browser.describe(Button.class, new ButtonDescription.Builder()
                 .buttonType("submit")
                 .name("Google Search")
                 .tagName("INPUT").build()).click();
 
-        Verify.isTrue(browser.describe(Page.class, new PageDescription()).getTitle().matches("^StormRunner Functional.*"));
+        Status p_f;
+        if (Verify.isTrue(browser.describe(Page.class, new PageDescription()).getTitle().matches("^"+myString+".*"))){
+            p_f = Status.Passed;
+        } else {
+            p_f = Status.Failed;
+        }
+        Reporter.reportEvent("Title Check", "<h1>"+myString+"</h1><br>is the title to validate", p_f);
 
     }
 
